52a2a92d019981b29150f379f8fff7e476248297
Chojo0 <j2017asc@gmail.com>
2025-04-05 21:05:45 -0500

    connecting DB - HTTP:GET

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,9 +1,18 @@
-/* Funcion que nos devuelve las categorias de la DB */
-const getCategorias =  (req, res) => {
-    res.json({"categorias": ["pescado", "carne", "huevos", "frutas", "verduras"]});
+import getConnection from '../db/database.js';
+
+/* Funcion asyncrona la cual nos devuelve la promesa de la base de datos */
+const getCategorias =  async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query('SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias');
+        res.json(result);
+    } catch (error) {
+        console.error("Time to panic");
+        
+    }     
 };
 
-/* Exportamos nuestros metodos HTTP */
+/* Exportamos nuestros metodos HTTP */ 
 export const methodHTTP = {
     getCategorias 
 };
